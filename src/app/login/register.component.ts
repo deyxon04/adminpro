a7e1a6d36b1839cf0f108aff36f90cdf4e36dd79
Deyxon Hinestroza <Deyxonh@gmail.com>
2019-01-21 15:21:55 -0500

    first commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  styleUrls:['./login.component.css'],
+  templateUrl: './register.component.html',
+  styles: []
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
