bbdf445273b648d96c95ef4bb3ee5edb381726e6
Deyxon Hinestroza <deyxonh@gmail.com>
2019-06-15 10:31:48 -0500

    modificaciones en los modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,29 @@
+import { Routes, RouterModule } from "@angular/router";
+import { PagesComponent } from "./pages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+import { ProgressComponent } from "./progress/progress.component";
+
+const pagesRoutes: Routes = [
+  {
+    component: PagesComponent,
+    path: "",
+    children: [
+      {
+        path: "dashboard",
+        component: DashboardComponent
+      },
+      {
+        path: "graficas1",
+        component: Graficas1Component
+      },
+      {
+        path: "progress",
+        component: ProgressComponent
+      },
+      { path: "", redirectTo: "/dashboard", pathMatch: "full" }
+    ]
+  }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild(pagesRoutes);
