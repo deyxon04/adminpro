dab11e5337a1adb2b487d3e10bc6ecf660217ca5
Deyxon Hinestroza <deyxonh@gmail.com>
2019-06-15 10:31:09 -0500

    modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
 
 //Rutas
-import { APP_ROUTES } from './app.routing';
-import { RegisterComponent } from './login/register.component';
-import { PagesModule } from './pages/pages.module';
-import { SharedModule } from './shared/shared.module';
-
+import { APP_ROUTES } from "./app.routing";
+import { RegisterComponent } from "./login/register.component";
+import { PagesModule } from "./pages/pages.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    APP_ROUTES,
-    PagesModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, APP_ROUTES, PagesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
